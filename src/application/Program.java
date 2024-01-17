67d867d2bbf93104508156165177f1659edbb485
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-01-17 14:32:57 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,5 +36,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "gergre", "gerrg@gmaiol.vcom", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("==== TEST 5: seller uptadate ====");
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Marta");
+		sellerDao.update(seller);
+		System.out.println(seller);
+		
 	}
 }
