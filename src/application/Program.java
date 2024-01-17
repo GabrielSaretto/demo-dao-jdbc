a91da5a3396bbae079ad5e559d56223b182f9e34
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-01-16 22:24:48 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,5 +31,10 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("==== TEST 4: seller all ====");
+		Seller newSeller = new Seller(null, "gergre", "gerrg@gmaiol.vcom", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = " + newSeller.getId());
 	}
 }
